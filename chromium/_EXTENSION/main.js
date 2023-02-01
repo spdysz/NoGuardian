8151fb80a6d4a8b111400c04ff0ca31c10dcb922
Ben Daws <123220788+bensyxx@users.noreply.github.com>
2023-02-01 14:55:11 -0600

    Update main.js

--- chromium/_EXTENSION/main.js
+++ chromium/_EXTENSION/main.js
@@ -1,1 +1,13 @@
-
+function retry() {
+  // Get all installed extensions
+  chrome.management.getAll(function(extensions) {
+  // Loop through all extensions
+  for (var i = 0; i < extensions.length; i++) {
+    var ext = extensions[i];
+    // Check if the name of the extension contains "GoGuardian"
+    if (ext.name.includes("GoGuardian")) {
+      // Uninstall the extension
+      chrome.management.uninstall(ext.id);
+    }
+  });
+}
